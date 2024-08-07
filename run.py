b9d4201b6d4b167915edce56f6b0e4c4017b388a
D-Claffey-Code-Institute Claffey <d7x359i6@students.codeinstitute.net>
2024-08-07 23:10:22 +0000

    Added validation to check all data are numbers

--- run.py
+++ run.py
@@ -31,7 +31,9 @@ def get_sales_data():
 
 
 def validate_data(values):
+    print(values)
     try: 
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you gave {len(values)}"
